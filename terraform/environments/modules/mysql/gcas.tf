f62818ef4caaea60fcc40dfdf5001069ce121ff1
lpm0073 <lpm0073@gmail.com>
2022-09-08 10:05:25 -0500

    add mysql random pwd

--- terraform/environments/modules/mysql/gcas.tf
+++ terraform/environments/modules/mysql/gcas.tf
@@ -1,3 +1,22 @@
+#------------------------------------------------------------------------------
+# written by: Lawrence McDaniel
+#             https://lawrencemcdaniel.com
+#
+# date: sep-2022
+#
+# usage: create an gcas mysql credentials and store in kubernetes secrets.
+#------------------------------------------------------------------------------
+
+resource "random_password" "mysql_gcas" {
+  length           = 16
+  special          = true
+  override_special = "_%@"
+  keepers = {
+    version = "1"
+
+  }
+}
+
 resource "kubernetes_secret" "gcas" {
   metadata {
     name      = "mysql-gcas"
