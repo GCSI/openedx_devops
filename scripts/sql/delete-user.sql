268b0d732330ae7d2b62ac688fdebd798bac1bec
lpm0073 <lpm0073@gmail.com>
2022-09-14 06:43:27 -0500

    add user tours

--- scripts/sql/delete-user.sql
+++ scripts/sql/delete-user.sql
@@ -7,258 +7,266 @@
  * usage:			delete a user
  *---------------------------------------------------------------------------*/
 SET @username = 'mcdaniel';
-use openedx;
+use codlp_staging_edx;
 
-DELETE FROM openedx.schedules_scheduleexperience
+DELETE FROM codlp_staging_edx.schedules_scheduleexperience
 WHERE	(id IN 	(
 				select id from (
 								SELECT se.id
-								FROM openedx.schedules_schedule s
-									JOIN openedx.schedules_scheduleexperience se on (s.id = se.schedule_id)
-									JOIN openedx.student_courseenrollment c ON (s.enrollment_id = c.id)
-									JOIN openedx.auth_user u ON (u.id = c.user_id)
+								FROM codlp_staging_edx.schedules_schedule s
+									JOIN codlp_staging_edx.schedules_scheduleexperience se on (s.id = se.schedule_id)
+									JOIN codlp_staging_edx.student_courseenrollment c ON (s.enrollment_id = c.id)
+									JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id)
 								WHERE	(u.username = @username)
                                 ) as d
                 )
 		);
 
-DELETE FROM openedx.schedules_schedule
+DELETE FROM codlp_staging_edx.schedules_schedule
 WHERE	(id IN 	(
 				select id from (
 								SELECT s.id
-								FROM openedx.schedules_schedule s
-									JOIN openedx.student_courseenrollment c ON (s.enrollment_id = c.id)
-									JOIN openedx.auth_user u ON (u.id = c.user_id)
+								FROM codlp_staging_edx.schedules_schedule s
+									JOIN codlp_staging_edx.student_courseenrollment c ON (s.enrollment_id = c.id)
+									JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id)
 								WHERE	(u.username = @username)
                                 ) as d
                 )
 		);
 
-DELETE FROM openedx.student_manualenrollmentaudit
+DELETE FROM codlp_staging_edx.student_manualenrollmentaudit
 WHERE	(id IN 	(
 				select id from (
 								SELECT s.id
-								FROM openedx.student_manualenrollmentaudit s
-									JOIN openedx.student_courseenrollment c ON (s.enrollment_id = c.id)
-									JOIN openedx.auth_user u ON (u.id = c.user_id)
+								FROM codlp_staging_edx.student_manualenrollmentaudit s
+									JOIN codlp_staging_edx.student_courseenrollment c ON (s.enrollment_id = c.id)
+									JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id)
 								WHERE	(u.username = @username)
                                 ) as d
                 )
 		);
 
 
-DELETE FROM openedx.user_api_usercoursetag
+DELETE FROM codlp_staging_edx.user_api_usercoursetag
 WHERE	(id IN 	(
 				select id from (
-								SELECT c.id FROM openedx.user_api_usercoursetag c JOIN openedx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+								SELECT c.id FROM codlp_staging_edx.user_api_usercoursetag c JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
 
-DELETE FROM openedx.courseware_studentmodule
+DELETE FROM codlp_staging_edx.courseware_studentmodule
 WHERE	(id IN 	(
 				select id from (
-								SELECT c.id FROM openedx.courseware_studentmodule c JOIN openedx.auth_user u ON (u.id = c.student_id) WHERE (u.username = @username)
+								SELECT c.id FROM codlp_staging_edx.courseware_studentmodule c JOIN codlp_staging_edx.auth_user u ON (u.id = c.student_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
-DELETE FROM openedx.student_manualenrollmentaudit
+DELETE FROM codlp_staging_edx.student_manualenrollmentaudit
 WHERE	(id IN 	(
 				select id from (
-								SELECT s.id FROM openedx.student_manualenrollmentaudit s JOIN openedx.auth_user u ON (u.id = s.enrolled_by_id) WHERE (u.username = @username)
-                                ) as d
-                )
-		);
-
-
-DELETE FROM openedx.student_courseenrollment
-WHERE	(id IN 	(
-				select id from (
-								SELECT s.id FROM openedx.student_courseenrollment s JOIN openedx.auth_user u ON (u.id = s.user_id) WHERE (u.username = @username)
+								SELECT s.id FROM codlp_staging_edx.student_manualenrollmentaudit s JOIN codlp_staging_edx.auth_user u ON (u.id = s.enrolled_by_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
 
-
-DELETE FROM	openedx.auth_registration
+DELETE FROM codlp_staging_edx.student_courseenrollment
 WHERE	(id IN 	(
 				select id from (
-								SELECT r.id FROM openedx.auth_registration r JOIN openedx.auth_user u ON (u.id = r.user_id) WHERE (u.username = @username)
-                                ) as d
-                )
-		);
-
-DELETE FROM	openedx.auth_userprofile
-WHERE	(id IN 	(
-				select id from (
-								SELECT p.id FROM openedx.auth_userprofile p JOIN openedx.auth_user u ON (u.id = p.user_id) WHERE (u.username = @username)
-                                ) as d
-                )
-		);
-
-DELETE FROM	openedx.completion_blockcompletion
-WHERE	(id IN 	(
-				select id from (
-								SELECT c.id FROM openedx.completion_blockcompletion c JOIN openedx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
-                                ) as d
-                )
-		);
-
-DELETE FROM	openedx.course_groups_cohortmembership
-WHERE	(id IN 	(
-				select id from (
-								SELECT c.id FROM openedx.course_groups_cohortmembership c JOIN openedx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
-                                ) as d
-                )
-		);
-
-DELETE FROM	openedx.course_groups_courseusergroup_users
-WHERE	(id IN 	(
-				select id from (
-								SELECT c.id FROM openedx.course_groups_courseusergroup_users c JOIN openedx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
-                                ) as d
-                )
-		);
-
-DELETE FROM	openedx.django_comment_client_role_users
-WHERE	(id IN 	(
-				select id from (
-								SELECT c.id FROM openedx.django_comment_client_role_users c JOIN openedx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
-                                ) as d
-                )
-		);
-
-DELETE FROM	openedx.experiments_experimentdata
-WHERE	(id IN 	(
-				select id from (
-								SELECT c.id FROM openedx.experiments_experimentdata c JOIN openedx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
-                                ) as d
-                )
-		);
-
-DELETE FROM	openedx.oauth2_provider_refreshtoken
-WHERE	(id IN 	(
-				select id from (
-								SELECT c.id FROM openedx.oauth2_provider_refreshtoken c JOIN openedx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+								SELECT s.id FROM codlp_staging_edx.student_courseenrollment s JOIN codlp_staging_edx.auth_user u ON (u.id = s.user_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
 
-DELETE FROM	openedx.oauth2_provider_accesstoken
+
+DELETE FROM	codlp_staging_edx.auth_registration
 WHERE	(id IN 	(
 				select id from (
-								SELECT c.id FROM openedx.oauth2_provider_accesstoken c JOIN openedx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+								SELECT r.id FROM codlp_staging_edx.auth_registration r JOIN codlp_staging_edx.auth_user u ON (u.id = r.user_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
-DELETE FROM	openedx.schedules_historicalschedule
+DELETE FROM	codlp_staging_edx.auth_userprofile
 WHERE	(id IN 	(
 				select id from (
-								SELECT c.id FROM openedx.schedules_historicalschedule c JOIN openedx.auth_user u ON (u.id = c.history_user_id) WHERE (u.username = @username)
+								SELECT p.id FROM codlp_staging_edx.auth_userprofile p JOIN codlp_staging_edx.auth_user u ON (u.id = p.user_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
-DELETE FROM	openedx.social_auth_usersocialauth
+DELETE FROM	codlp_staging_edx.completion_blockcompletion
 WHERE	(id IN 	(
 				select id from (
-								SELECT c.id FROM openedx.social_auth_usersocialauth c JOIN openedx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+								SELECT c.id FROM codlp_staging_edx.completion_blockcompletion c JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
-DELETE FROM	openedx.student_anonymoususerid
+DELETE FROM	codlp_staging_edx.course_groups_cohortmembership
 WHERE	(id IN 	(
 				select id from (
-								SELECT c.id FROM openedx.student_anonymoususerid c JOIN openedx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+								SELECT c.id FROM codlp_staging_edx.course_groups_cohortmembership c JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
-DELETE FROM	openedx.student_courseenrollment_history
+DELETE FROM	codlp_staging_edx.course_groups_courseusergroup_users
 WHERE	(id IN 	(
 				select id from (
-								SELECT c.id FROM openedx.student_courseenrollment_history c JOIN openedx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+								SELECT c.id FROM codlp_staging_edx.course_groups_courseusergroup_users c JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
-DELETE FROM	openedx.student_courseenrollment_history
+DELETE FROM	codlp_staging_edx.django_comment_client_role_users
 WHERE	(id IN 	(
 				select id from (
-								SELECT c.id FROM openedx.student_courseenrollment_history c JOIN openedx.auth_user u ON (u.id = c.history_user_id) WHERE (u.username = @username)
+								SELECT c.id FROM codlp_staging_edx.django_comment_client_role_users c JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
-DELETE FROM	openedx.student_userattribute
+DELETE FROM	codlp_staging_edx.experiments_experimentdata
 WHERE	(id IN 	(
 				select id from (
-								SELECT c.id FROM openedx.student_userattribute c JOIN openedx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+								SELECT c.id FROM codlp_staging_edx.experiments_experimentdata c JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
-DELETE FROM	openedx.user_api_userpreference
+DELETE FROM	codlp_staging_edx.oauth2_provider_refreshtoken
 WHERE	(id IN 	(
 				select id from (
-								SELECT c.id FROM openedx.user_api_userpreference c JOIN openedx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+								SELECT c.id FROM codlp_staging_edx.oauth2_provider_refreshtoken c JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
-DELETE FROM	openedx.verify_student_ssoverification
+
+DELETE FROM	codlp_staging_edx.oauth2_provider_accesstoken
 WHERE	(id IN 	(
 				select id from (
-								SELECT c.id FROM openedx.verify_student_ssoverification c JOIN openedx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+								SELECT c.id FROM codlp_staging_edx.oauth2_provider_accesstoken c JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
-DELETE FROM	openedx.course_creators_coursecreator
+DELETE FROM	codlp_staging_edx.schedules_historicalschedule
 WHERE	(id IN 	(
 				select id from (
-								SELECT c.id FROM openedx.course_creators_coursecreator c JOIN openedx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+								SELECT c.id FROM codlp_staging_edx.schedules_historicalschedule c JOIN codlp_staging_edx.auth_user u ON (u.id = c.history_user_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
-DELETE FROM	openedx.course_overviews_historicalcourseoverview
+DELETE FROM	codlp_staging_edx.social_auth_usersocialauth
+WHERE	(id IN 	(
+				select id from (
+								SELECT c.id FROM codlp_staging_edx.social_auth_usersocialauth c JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+                                ) as d
+                )
+		);
+
+DELETE FROM	codlp_staging_edx.student_anonymoususerid
+WHERE	(id IN 	(
+				select id from (
+								SELECT c.id FROM codlp_staging_edx.student_anonymoususerid c JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+                                ) as d
+                )
+		);
+
+DELETE FROM	codlp_staging_edx.student_courseenrollment_history
+WHERE	(id IN 	(
+				select id from (
+								SELECT c.id FROM codlp_staging_edx.student_courseenrollment_history c JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+                                ) as d
+                )
+		);
+
+DELETE FROM	codlp_staging_edx.student_courseenrollment_history
+WHERE	(id IN 	(
+				select id from (
+								SELECT c.id FROM codlp_staging_edx.student_courseenrollment_history c JOIN codlp_staging_edx.auth_user u ON (u.id = c.history_user_id) WHERE (u.username = @username)
+                                ) as d
+                )
+		);
+
+DELETE FROM	codlp_staging_edx.student_userattribute
+WHERE	(id IN 	(
+				select id from (
+								SELECT c.id FROM codlp_staging_edx.student_userattribute c JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+                                ) as d
+                )
+		);
+
+DELETE FROM	codlp_staging_edx.user_api_userpreference
+WHERE	(id IN 	(
+				select id from (
+								SELECT c.id FROM codlp_staging_edx.user_api_userpreference c JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+                                ) as d
+                )
+		);
+
+DELETE FROM	codlp_staging_edx.verify_student_ssoverification
+WHERE	(id IN 	(
+				select id from (
+								SELECT c.id FROM codlp_staging_edx.verify_student_ssoverification c JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+                                ) as d
+                )
+		);
+
+DELETE FROM	codlp_staging_edx.course_creators_coursecreator
+WHERE	(id IN 	(
+				select id from (
+								SELECT c.id FROM codlp_staging_edx.course_creators_coursecreator c JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+                                ) as d
+                )
+		);
+
+DELETE FROM	codlp_staging_edx.course_overviews_historicalcourseoverview
 WHERE	(history_user_id IN 	(
 				select history_user_id from (
-								SELECT c.history_user_id FROM openedx.course_overviews_historicalcourseoverview c JOIN openedx.auth_user u ON (u.id = c.history_user_id) WHERE (u.username = @username)
+								SELECT c.history_user_id FROM codlp_staging_edx.course_overviews_historicalcourseoverview c JOIN codlp_staging_edx.auth_user u ON (u.id = c.history_user_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
-DELETE FROM	openedx.gcsi_cms_coursechangelog
+DELETE FROM	codlp_staging_edx.gcsi_cms_coursechangelog
 WHERE	(id IN 	(
 				select id from (
-								SELECT c.id FROM openedx.gcsi_cms_coursechangelog c JOIN openedx.auth_user u ON (u.id = c.published_by_id) WHERE (u.username = @username)
+								SELECT c.id FROM codlp_staging_edx.gcsi_cms_coursechangelog c JOIN codlp_staging_edx.auth_user u ON (u.id = c.published_by_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
-DELETE FROM	openedx.gcsi_cms_coursechangelog
+DELETE FROM	codlp_staging_edx.gcsi_cms_coursechangelog
 WHERE	(id IN 	(
 				select id from (
-								SELECT c.id FROM openedx.gcsi_cms_coursechangelog c JOIN openedx.auth_user u ON (u.id = c.edited_by_id) WHERE (u.username = @username)
+								SELECT c.id FROM codlp_staging_edx.gcsi_cms_coursechangelog c JOIN codlp_staging_edx.auth_user u ON (u.id = c.edited_by_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
-DELETE FROM	openedx.student_courseaccessrole
+DELETE FROM	codlp_staging_edx.student_courseaccessrole
 WHERE	(id IN 	(
 				select id from (
-								SELECT c.id FROM openedx.student_courseaccessrole c JOIN openedx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
+								SELECT c.id FROM codlp_staging_edx.student_courseaccessrole c JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id) WHERE (u.username = @username)
                                 ) as d
                 )
 		);
 
+DELETE FROM	codlp_staging_edx.user_tours_usertour
+WHERE	(id IN 	(
+				select id from (
+								SELECT c.id FROM codlp_staging_edx.user_tours_usertour c JOIN codlp_staging_edx.auth_user u ON (u.id = c.user_id) WHERE (u.username = 'Tauriq')
+                                ) as d
+                )
+		);
+ 
 
-DELETE FROM	openedx.auth_user WHERE (username = @username);
+DELETE FROM	codlp_staging_edx.auth_user WHERE (username = @username);
